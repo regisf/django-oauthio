d3baa58b0036c92dd5d60cdcd8d253554884bbc8
regisf <regisfloret@gmail.com>
2015-02-10 14:59:46 +0100

    Javascript optimization and improvments

--- static/oauthio.js
+++ static/oauthio.js
@@ -1,66 +1,104 @@
 /**
- * Created by  on 30/01/2015.
+ * Django-OAuthio - Use OAuth.io authentication service with Django
+ * (c) Régis FLORET 2015 and later
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
-
 var OAuthIOStart = (function() {
     "use strict";
 
-    function _register_on_server(response) {
-        if (response.status && response.status === 'success') {
-	    var request = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-            request.addEventlistener('load', function() {
+    // Setup default behaviour
+    var options_ = {
+        // Connected with the provider
+        done: function(provider, result) {
+            if (result.status && result.status === 'success') {
+                var request = new XMLHttpRequest();
+                var self=this;
 
-            }, false);
+                request.onerror = function (e) {self.error(provider, e.responseText)};
 
-            request.onload = function(evt) {
+                request.onreadystatechange = function (e) {
+                    if (request.readyState === 4) {
+                        if (request.status === 200) {
+                            self.success(provider, result);
+                        } else {
+                            self.error(provider, e.responseText);
+                        }
+                    }
+                };
 
-            };
-            request.open('POST', url);
-            request.send(JSON.stringify(response.data));
-	    
-	}
+                request.open('POST', url);
+                request.send(JSON.stringify(result.data));
+            }
+        },
 
+        // An error occured
+        fail: function(provider, err) {console.error(this.error(provider, err))},
 
-    }
+        // Reload page
+        success: function(provider, result) {document.location = document.location},
 
-    function _do_err(cb, err) {
-        if (cb && typeof cb == 'function') {
-            cb(false, result);
-        } else {
-            console.log(err);
+        // Display error on console
+        error: function(provider, err) {console.error("For provider:"+ provider + "Error: " + err)}
+    };
+
+    return function (options) {
+        if (options.public_key === undefined) {
+            if (options.debug === true) {
+                alert("OAuth.io public key is not set");
+            }
+            return;
         }
-    }
 
-    function OAuthIOStart(options) {
+        // Merge the objects
+        for(var o in options) {
+            options_[o] = options[o];
+        }
+
+        // Start OAuth.io library
         OAuth.initialize(options.public_key);
-        var oauths = document.querySelectorAll('[data-oauthio-provider]'), provider;
-        for (var i in oauths) {
+
+        // Get all element for connection.
+        var elements = document.querySelectorAll('[data-oauthio-provider]'), provider;
+        for (var i in elements) {
             (function (el) {
-                if (typeof el !== 'object') {
-                } else {
-                    el.addEventListener('click', function (event) {
+                if (typeof el === 'object') {
+                    el.onclick = function (event) {
                         event.preventDefault();
                         var provider = el.getAttribute("data-oauthio-provider");
                         OAuth.popup(provider)
                             .done(function (result) {
-                                if (options[provider] && typeof options[provider] == 'function') {
-                                    options[provider](false, provider, result);
-                                } else {
-                                    result.me()
-                                        .done(_register_on_server)
-                                        .fail(function(err) {
-                                            _do_err(config[provider], error);
-                                        });
-                                }
+                                result.me()
+                                    // OK Everythings goes fine
+                                    .done(function(result) {
+                                        options_.done(provider, result);
+                                    })
+
+                                    // The me() request failed
+                                    .fail(function(err) {options_.fail(provider, err)});
                             })
-                            .fail(function (err) {
-                                _do_err(options[provider], err);
-                            });
-                    });
+                            // The popup failed
+                            .fail(function(err) {options_.fail(provider, err)});
+                    };
                 }
-            })(oauths[i]);
+            })(elements[i]);
         }
     }
-    return OAuthIOStart;
 })();
 

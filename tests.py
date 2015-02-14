3ffc889734be3a1baf62e9e3350ffcb8d2eb83d4
regisf <regisfloret@gmail.com>
2015-02-14 14:57:51 +0100

    Add MIT license on all files and file docstring

--- tests.py
+++ tests.py
@@ -1,9 +1,35 @@
-__author__ = 'Regis'
+# -*- coding: UTF-8 -*-
+# OAuth.io service for Django
+# (c) Régis FLORET 2015 and later
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""
+Test the app
+"""
+
+__author__ = 'Regis FLORET'
+__version__ = '1.0'
+__license__ = 'MIT'
 
 import json
-
 import unittest
-
 from django.test.client import RequestFactory
 
 from .views import convert_request_to_json
@@ -22,7 +48,7 @@ class ConvertJsonTest(unittest.TestCase):
 
     def test_get_exists_with_json_query(self):
         factory = RequestFactory()
-        request = factory.get('/', {'json' : json.dumps(self.data)})
+        request = factory.get('/', {'json': json.dumps(self.data)})
         result = convert_request_to_json(request)
 
         self.assertIsInstance(result, dict)
@@ -45,7 +71,7 @@ class ConvertJsonTest(unittest.TestCase):
 
     def test_post_with_json_query(self):
         factory = RequestFactory()
-        request = factory.post('/', {'json' : json.dumps(self.data)})
+        request = factory.post('/', {'json': json.dumps(self.data)})
         result = convert_request_to_json(request)
 
         self.assertIsInstance(result, dict)
@@ -59,10 +85,6 @@ class ConvertJsonTest(unittest.TestCase):
 
         self.assertIsInstance(result, dict)
         self.assertEqual(result, self.data)
-
-
-class JSONMixinTest(unittest.TestCase):
-    pass
 
 
 if __name__ == '__main__':
